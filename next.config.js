6d911b62b59721100b9ba1a9f9fadd0b7e6aa35d
Jari Peltola <jena9988@gmail.com>
2026-04-28 18:21:17 +0300

    Domain destination

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'finishpoint.fi' }],
+        destination: 'https://pakuvie.fi/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.finishpoint.fi' }],
+        destination: 'https://pakuvie.fi/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
